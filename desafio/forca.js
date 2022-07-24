3ecf26e10bb5df18aade7b465deb0884d9f8d652
Otavio <otaviomiranda@usp.br>
2022-07-24 19:08:19 -0300

    adiciona regras

--- desafio/forca.js
+++ desafio/forca.js
@@ -10,20 +10,38 @@ class Forca {
    }
 
   buscarEstado() {
+    //O jogo deve iniciar com o estado aguardando chute.
+    let estado = "aguardando chute";
+
     // Caso a quantidade de vidas chegue a 0 (zero), o estado do jogo deve mudar para perdeu.
     return vidas == 0 && "perdeu"  
 
     //Caso a quantidade de vidas seja maior que zero e o jogador acerte a última letra, o estado do jogo deve mudar para ganhou.
+    // FAZER POR ARRAY E VERIFICAR QUANDO ELE ESTIVER VAZIO
     return vidas > 0 && palavra.includes(letrasChutadas[letrasChutadas.length - 1]) && "ganhou" // CONFERIR CÓDIGO!!!!!
     return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
     //Se a letra chutada não estiver contida na palavra, deve subtrair uma vida
-
     letrasChutadas.forEach(letra => {
       if (!palavra.includes(letra)) {
         vidas --;
       }
+
+      // Se a letra chutada estiver contida na palavra, deve ser substituida na "palavra" em sua 
+      //respectiva posição. Ex.: A palavra secreta é "bala" e o jogador chutou a letra "b",
+      // então a palavra que é retornada no método buscarDadosDoJogo, deve ser ["b", "", "", "_" ].
+      palavraAlvo = palavra.split("");
+      palavra.forEach(() => palavraAlvo.push('_'));
+      
+      //Duplicar array: Original será o visualizado com "_", enquanto a cópia será o descartado para conferencia.
+      
+      palavraAlvo.forEach((letraDaPalavraAlvo)=> {
+        if (letra === letraDaPalavraAlvo) {
+          //?????
+        }
+      })
+
     })
       return {
           letrasChutadas: [], // Deve conter todas as letras chutadas
